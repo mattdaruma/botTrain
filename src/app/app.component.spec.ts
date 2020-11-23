93137bc81dd17f690aaf12bddacc11c3c5e1b7b1
MattDaruma <jerrymatthewmorris@icloud.com>
2020-11-23 14:08:31 -0700

    first commit, about to change strategies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'botTrain'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('botTrain');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { BotManagerService } from './bot-manager.service';
+import { Component, ViewChild } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'botTrain';
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  CurrentPage = 'Upload';
+  NoData: boolean;
+  NoBot: boolean;
+  NoTraining: boolean;
+
+  constructor(private botManager: BotManagerService){
+    this.updateUi();
+    this.botManager.botChanged.subscribe(botChanged => {
+      if (botChanged){
+        this.updateUi();
+      }
+    })
+  }
+  updateUi(): void{
+    console.log('thissource', this.botManager.source)
+    this.NoData = this.botManager.source.length === 0;
+    this.NoBot = this.botManager.bot.predictor.length === 0 || this.botManager.trainers.length === 0;
+    this.NoTraining = this.botManager.training.length === 0;
+    if (this.NoData){ this.CurrentPage = 'Upload'; }
+    if (!this.NoData && this.NoBot){ this.CurrentPage = 'Configure'; }
+  }
+
+  navigate(pageName: string): void {
+    this.CurrentPage = pageName;
+    this.close();
+  }
+  close(): void {
+    this.sidenav.close();
+  }
+  deleteBot(): void{
+    this.botManager.emptyCache('bot');
+  }
+  deleteData(): void{
+    this.botManager.emptyCache('data');
+  }
+  deleteTrain(): void{
+    this.botManager.emptyCache('train');
+  }
 }
